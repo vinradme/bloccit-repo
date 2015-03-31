8619f5ff4c97da6512db797e5d7159640f5dda54
Vinodhini Ravikumar <vinradme@gmail.com>
2015-03-30 22:26:10 -0700

    associations-checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
 require 'faker'
 
+# Create Users
+
+5.times do 
+  user=User.new(
+      name:   Faker::Name.name,
+      email:  Faker::Internet.email,
+      password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
 # Create Posts
 50.times do 
   Post.create!(
+    user:  users.sample, 
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -17,6 +30,14 @@ posts = Post.all
     )
 end
 
+user= User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: "vinradme@gmail.com",
+  password: "helloworld"
+  )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
