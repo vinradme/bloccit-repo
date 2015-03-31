216bc95f51876c5ca45288ef1e30268f17c5ee1f
Vinodhini Ravikumar <vinradme@gmail.com>
2015-03-30 19:28:28 -0700

    Completed authentication continued assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :flash_attack
+  
+    protected
+    
+    def flash_attack
+      flash[:notice] = "This is displayed before every post view"
+    end
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
