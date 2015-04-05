6822b1474d946739ba8797e4141c532db6de6742
Vinodhini Ravikumar <vinradme@gmail.com>
2015-04-05 13:56:57 -0700

    Associate and nested topics and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 require 'faker'
 
-# Create Users
+#Create topics
 
+15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+
+topics = Topic.all
+
+# Create Users
 5.times do 
   user=User.new(
       name:   Faker::Name.name,
@@ -15,6 +25,7 @@ users = User.all
 # Create Posts
 50.times do 
   Post.create!(
+    topic: topics.sample,
     user:  users.sample, 
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
@@ -30,14 +41,35 @@ posts = Post.all
     )
 end
 
-user= User.first
-user.skip_reconfirmation!
-user.update_attributes!(
-  email: "vinradme@gmail.com",
+admin = User.new(
+  name: "Admin User",
+  email: "admin@example.com",
+  password: "helloworld",
+  role: "admin"
+  )
+admin.skip_confirmation!
+admin.save!
+
+moderator=User.new(
+  name: "Moderator User",
+  email: "moderator@example.com",
+  password: "helloworld",
+  role: "moderator"
+  )
+moderator.skip_confirmation!
+moderator.save!
+
+member=User.new(
+  name: "Member User",
+  email: "member@example.com",
   password: "helloworld"
   )
+member.skip_confirmation!
+member.save!
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
