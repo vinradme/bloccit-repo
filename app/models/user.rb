f92b218e1eb2d307d10631487e22111b56769297
Vinodhini Ravikumar <vinradme@gmail.com>
2015-04-09 15:16:45 -0700

    Updating db migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
-  mount_uploader :avatar, AvatarUploader
 
   def admin?
     role == 'admin'
@@ -13,5 +12,7 @@ class User < ActiveRecord::Base
   def moderator?
     role == 'moderator'
   end
+
+  mount_uploader :avatar, AvatarUploader
   
 end
