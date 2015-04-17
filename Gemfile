0c332397c8c2d71e8ef67d8e85ee83ab6ea3e587
Vinodhini Ravikumar <vinradme@gmail.com>
2015-04-16 21:03:39 -0700

    Completed pagination checkpoint

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'fog'
 
+gem 'will_paginate', '~> 3.0.5'
+
 group :production do
   gem 'pg'
   gem 'rails_12factor'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,6 +44,8 @@ class PostsController < ApplicationController
     end
   end
 
+  private
+  
   def post_params
     params.require(:post).permit(:title, :body)
   end
